9897643218fb1c6c9295ec9135324a8fb8ddf697
MrJam <chengcheng12138@sina.cn>
2016-09-25 01:22:11 +0800

    update

--- app/src/main/java/cn/com/yjpay/gittestapp/MainActivity.java
+++ app/src/main/java/cn/com/yjpay/gittestapp/MainActivity.java
@@ -2,12 +2,74 @@ package cn.com.yjpay.gittestapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.ListViewCompat;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
 
 public class MainActivity extends AppCompatActivity {
-
+    private ListView listView;
+    private List<String> data;
+    private ListAdapter adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        listView = (ListView) findViewById(R.id.listView);
+        data = new ArrayList<>();
+        for (int i = 0 ;i<50;i++){
+            data.add(i,"第"+(i+0)+"行代码!");
+        }
+        adapter = new ListAdapter();
+        listView.setAdapter(adapter);
+
+    }
+
+
+    private class ListAdapter extends BaseAdapter{
+
+        @Override
+        public int getCount() {
+            return data.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return data.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            ViewHolder viewHolder = null;
+            if (convertView==null){
+                convertView = getLayoutInflater().inflate(R.layout.list_item,null);
+                viewHolder = new ViewHolder();
+                viewHolder.textView = (TextView) convertView.findViewById(R.id.textView);
+                convertView.setTag(viewHolder);
+            }else{
+                viewHolder = (ViewHolder) convertView.getTag();
+            }
+            viewHolder.textView.setText(data.get(position));
+            return convertView;
+        }
+
+        class ViewHolder{
+            TextView textView;
+        }
     }
 }
