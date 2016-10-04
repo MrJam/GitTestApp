b9b080a05d182331993f8859f351519afa237143
HuLiangCheng <chengcheng12138@sina.cn>
2016-10-04 22:02:18 +0800

    添加代码

--- activitylaunchmode/src/main/java/com/example/activitylaunchmode/SecondActivity.java
+++ activitylaunchmode/src/main/java/com/example/activitylaunchmode/SecondActivity.java
@@ -1,7 +1,9 @@
 package com.example.activitylaunchmode;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class SecondActivity extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class SecondActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+    }
+
+    public void startActivity(View v){
+        startActivity(new Intent(this,ThirdActivity.class));
     }
 }
--- activitylaunchmode/src/main/java/com/example/activitylaunchmode/ThirdActivity.java
+++ activitylaunchmode/src/main/java/com/example/activitylaunchmode/ThirdActivity.java
@@ -1,7 +1,9 @@
 package com.example.activitylaunchmode;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class ThirdActivity extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class ThirdActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_third);
+    }
+
+    public void startActivity(View v){
+        startActivity(new Intent(this,MainActivity.class));
     }
 }
